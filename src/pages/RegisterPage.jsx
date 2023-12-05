b051117e09ee5d0c01e7236a98687ec84d3b47ab
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2023-12-05 20:56:58 +0900

    💄 style : 로그인 회원가입 css 추가

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import Button from '../components/UI/Button';
 import {createUserWithEmailAndPassword, updateProfile} from 'firebase/auth';
 import {auth} from '../shared/firebase';
+import styled from 'styled-components';
+import {useNavigate} from 'react-router-dom';
 
 const Register = () => {
+  const navigate = useNavigate();
   const [inputs, setInputs] = useState({
     email: '',
     nickname: '',
@@ -17,7 +20,7 @@ const Register = () => {
     });
   };
 
-  const submitHandler = async e => {
+  const registerUser = async e => {
     e.preventDefault();
     const defaultPhotoUrl = 'https://picpac.kr/common/img/default_profile.png';
     try {
@@ -35,16 +38,73 @@ const Register = () => {
     });
   };
 
+  const moveToLoginPage = e => {
+    e.preventDefault();
+    navigate('/login');
+  };
+
   return (
-    <>
-      <form onSubmit={submitHandler}>
+    <ScWrapper>
+      <ScForm>
+        <h1>회원가입</h1>
         <input type="text" placeholder="이메일" name="email" value={inputs.email} onChange={changeInputs} />
         <input type="text" placeholder="닉네임" name="nickname" value={inputs.nickname} onChange={changeInputs} />
         <input type="password" placeholder="비밀번호" name="password" value={inputs.password} onChange={changeInputs} />
-        <Button>회원가입</Button>
-      </form>
-    </>
+        <Button onClick={registerUser}>회원가입</Button>
+        <span onClick={moveToLoginPage}>로그인 하러가기</span>
+      </ScForm>
+    </ScWrapper>
   );
 };
+
+const ScWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const ScForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 40%;
+  height: 50%;
+  padding: 30px;
+  border-radius: 10px;
+  background-color: var(--light-blue);
+
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+
+  input {
+    width: 50%;
+    border: none;
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+
+  input:nth-child(4) {
+    margin-bottom: 20px;
+  }
+
+  button {
+    width: 50%;
+    margin-bottom: 10px;
+  }
+
+  span {
+    margin-top: 10px;
+    cursor: pointer;
+    color: var(--deep-blue);
+    font-weight: bold;
+  }
+`;
 
 export default Register;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Detail from '../components/UI/Detail';
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/Detail/:id" element={<Detail />} />
-        <Route />
       </Routes>
     </BrowserRouter>
   );
