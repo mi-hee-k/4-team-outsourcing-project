2430032f8550b9b36f4b6d32ecbd61293168714f
손지원 <sonjiwon@sonjiwon-ui-MacBookAir.local>
2023-12-06 11:04:32 +0900

    🎉 add: EditDetailPage 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Router from './shared/Router';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      <Router />
+    </div>
+  );
 }
 
 export default App;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Detail from '../components/UI/Detail';
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import DetailPage from '../pages/DetailPage';
+import EditDetailPage from '../pages/EditDetailPage';
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Detail/:id" element={<Detail />} />
-        <Route />
+        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path="/adddetail/:id" element={<EditDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
