1dc373f5c38738d14194f88a730e123d2e16a30f
hanjiwoo <hanmingyu3@naver.com>
2023-12-09 22:10:58 +0900

    안쓰는 로직 삭제

--- src/components/UI/CustomHook.jsx
+++ src/components/UI/CustomHook.jsx
@@ -5,21 +5,8 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useEffect} from 'react';
 import Button from './Button';
 import {toast} from 'react-toastify';
-// export const addFirestore = async () => {
-//   try {
-//     const docRef = await addDoc(collection(db, 'users'), {
-//       first: 'Ada',
-//       last: 'Lovelace',
-//       born: 1815,
-//     });
-//     console.log('이게 니아이디', docRef.id);
-//   } catch (e) {
-//     console.error('추가작업에러발생', e);
-//   }
-// };
 
 export const useReadFirestore = async () => {
-  // console.log('이게 리스트', addlist);
   const dispatch = useDispatch();
   const dataReading = async () => {
     const querySnapshot = await getDocs(collection(db, 'fixs'));
@@ -43,21 +30,11 @@ export const useReadFirestore = async () => {
   }, []);
 };
 
-// export const Update = (photoURL, displayName, filteredList, dispatch) => {
-//   const lastList = filteredList.forEach(async item => {
-//     await setDoc(doc(db, 'fix', `${item.id}`), {
-//       ...doc,
-//       displayName,
-//       photoURL,
-//     });
-//   });
-//   dispatch(setList(lastList));
-// };
 export const Update = async (photoURL, displayName, filteredList, dispatch, dataReading) => {
   try {
     const updatePromises = filteredList.map(async item => {
       await setDoc(doc(db, 'fixs', `${item.id}`), {
-        ...item, // 이전 데이터를 모두 유지한 채 업데이트
+        ...item,
         displayName,
         photoURL,
       });
@@ -65,7 +42,7 @@ export const Update = async (photoURL, displayName, filteredList, dispatch, data
 
     await Promise.all(updatePromises);
     dataReading();
-    // 모든 문서 업데이트가 완료되면 상태를 업데이트
+
     dispatch(
       setList(
         filteredList.map(item => ({
@@ -89,25 +66,6 @@ export const Update = async (photoURL, displayName, filteredList, dispatch, data
     console.error('업데이트 중 오류 발생:', error);
   }
 };
-// export const deliteFirestore = async () => {
-//   await deleteDoc(doc(db, 'cities', 'DC'));
-// };
-// export const DataReading = async () => {
-
-//   const querySnapshot = await getDocs(collection(db, 'fixs'));
-//   let dataArr = [];
-//   querySnapshot.forEach(doc => {
-//     const data = doc.data();
-
-//     // console.log(data, ' 이게 독 아이디');
-//     dataArr.push({...data, id: doc.id});
-
-//     // console.log(data.createdAt, '이게그거');
-//     dataArr = dataArr.sort((a, b) => b.createdAt - a.createdAt);
-//   });
-
-//   dispatch(setList(dataArr));
-// };
 
 export default function EditBtn() {
   const list = useSelector(state => state.fixList);
